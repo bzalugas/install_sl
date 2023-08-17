040f94452a8e39e9292727e9766acbc38d5e5b47
bazaluga <bazaluga@student.42.fr>
2023-08-17 18:51:36 +0200

    update isl

--- isl.sh
+++ isl.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-git clone https://github.com/mtoyoda/sl.git ~/.sl && make -C ~/.sl && echo "alias 'ls=~/.sl/sl'" >> ~/.zshrc && alias 'ls=~/.sl/sl && ls'
+git clone https://github.com/mtoyoda/sl.git ~/.sl; make -C ~/.sl; echo "alias 'ls=~/.sl/sl && echo \$MSG && ls'" >> ~/.zshrc; echo "export MSG=\".............MASDAKISATION DE TA SESSION............\"" >> ~/.zshrc
